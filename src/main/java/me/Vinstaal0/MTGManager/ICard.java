74606c40fb2669af7ebc1e67ccbfa8def0137dcb
Vinstaal0 <vincent-staaltjens@live.nl>
2017-07-24 13:24:36 +0200

    Update ICard.java

--- src/main/java/me/Vinstaal0/MTGManager/ICard.java
+++ src/main/java/me/Vinstaal0/MTGManager/ICard.java
@@ -1,5 +1,42 @@
 package me.Vinstaal0.MTGManager;
 
 public interface ICard {
-
+	
+	int cmc;
+	ManaCost manaCost;
+	Color[] cardColorID;
+	Location location;
+	
+	default void setCmc(int cmc) {
+		this.cmc = cmc;
+	}
+	
+	default void setManaCost(ManaCost manaCost) {
+		this.manaCost = manaCost;	
+	}
+	
+	default void setCarColorID(Color... cardColorID) {
+		this.cardColorID = cardColorID;	
+	}
+	
+	default void setLocation(Location location) {
+		this.location = location;	
+	}
+	
+	default int getCmc() {
+		return this.cmc;
+	}
+	
+	default ManaCost getManaCost() {
+		return this.manaCost;	
+	}
+	
+	default Color[] getCardColorID() {
+		return this.cardColorID;	
+	}
+	
+	default Location getLocation() {
+		return this.location;
+	}
+	
 }
